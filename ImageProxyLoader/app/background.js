5cf864255f5b2f363370d913aeef6a4479f8994f
Bo Flynn <boflynn@gmail.com>
2015-01-24 17:58:36 -0500

    Initial commit.

---
+++ ImageProxyLoader/app/background.js
@@ -0,0 +1,99 @@
+﻿// Backers to setters and getters
+var _proxyUrl = '';
+var _urls = '';
+var _enabled = false;
+
+// Constants
+var _domainRegEx = /(.[^:]+):\/\/(.[^/]+)/;
+var _listenerFilters = {
+    urls: [
+        '*://*/*'
+    ],
+    types: ['image']
+};
+var _extraInfoSpec = ['blocking'];
+
+// Global settings object
+var settings = {
+    get proxyUrl() { return _proxyUrl; },
+    set proxyUrl(val) { _proxyUrl = val; },
+    get urls() { return _urls; },
+    set urls(val) {
+        if (val) {
+            _urls = val.split('\n');
+        } else {
+            _urls = '';
+        }
+    },
+    get enabled() { return _enabled; },
+    set enabled(val) {
+        _enabled = val;
+
+        // Icons small tweak from http://www.iconshock.com/android-icons/
+        var imgUrl = _enabled ? 'on38.png' : 'off38.png';
+        chrome.browserAction.setIcon({ path: 'img/' + imgUrl });
+
+        var hasListener = chrome.webRequest.onBeforeRequest.hasListener(getRedirectInformation);
+
+        if (_enabled && !hasListener) {
+            addListener();
+        }
+        else if (!_enabled && hasListener) {
+            removeListener();
+        }
+    }
+};
+
+function loadSettingsFromChromeStorage() {
+    chrome.storage.sync.get({
+        enabled: false,
+        proxyUrl: '',
+        urls: ''
+    }, function (items) {
+        settings.proxyUrl = items.proxyUrl;
+        settings.enabled = items.enabled;
+        settings.urls = items.urls;
+    });
+}
+
+function addListener() {
+    chrome.webRequest.onBeforeRequest.addListener(getRedirectInformation, _listenerFilters, _extraInfoSpec);
+}
+
+function removeListener() {
+    chrome.webRequest.onBeforeRequest.removeListener(getRedirectInformation);
+}
+
+function getRedirectInformation(info) {
+    if (!(settings.enabled && settings.proxyUrl && settings.urls)) {
+        return;
+    }
+
+    var originalUrl = info.url;
+    var domain = originalUrl.match(_domainRegEx)[2];
+
+    var isRedirectedDomain = contains(settings.urls, domain);
+
+    if (isRedirectedDomain) {
+        var newUrl = settings.proxyUrl.replace('$URL', originalUrl);
+
+        return {
+            redirectUrl: newUrl
+        };
+    }
+
+    return;
+}
+
+function contains(array, itemToFind) {
+    var i = array.length;
+    while (i--) {
+        if (array[i] === itemToFind) {
+            return true;
+        }
+    }
+    return false;
+}
+
+loadSettingsFromChromeStorage();
+addListener();
---
+++ ImageProxyLoader/app/options.js
@@ -0,0 +1,82 @@
+﻿// Cached DOM elements
+var proxyUrlElement = document.getElementById('proxyUrl');
+var urlsElement = document.getElementById('urls');
+var statusElement = document.getElementById('status');
+var onOffSwitchElement = document.getElementById('myonoffswitch');
+
+// Restores options stored in chrome.storage.
+function restoreOptions() {
+    chrome.storage.sync.get({
+        enabled: false,
+        proxyUrl: 'http://MyProxy.com?url=$URL',
+        urls: 'i.imgur.com\ni.stack.imgur.com'
+    }, function (items) {
+        onOffSwitchElement.checked = items.enabled;
+        proxyUrlElement.value = items.proxyUrl;
+        urlsElement.value = items.urls;
+    });
+}
+
+// Saves options to chrome.storage
+function saveOptions() {
+    var isValid = true;
+    var proxyUrl = proxyUrlElement.value;
+    var urls = urlsElement.value;
+    var enabled = onOffSwitchElement.checked;
+
+    // Don't bother with validation unless proxy is enabled
+    if (enabled) {
+        if (proxyUrl.indexOf('$URL') === -1) {
+            proxyUrlElement.classList.add('invalid');
+
+            isValid = false;
+        } else {
+            proxyUrlElement.classList.remove('invalid');
+        }
+
+        if (urls.indexOf('/') !== -1) {
+            urlsElement.classList.add('invalid');
+
+            isValid = false;
+        } else {
+            urlsElement.classList.remove('invalid');
+        }
+    }
+
+    // If no errors, save settings
+    if (isValid) {
+        chrome.storage.sync.set({
+            enabled: enabled,
+            proxyUrl: proxyUrl,
+            urls: urls
+        }, function () {
+            statusElement.classList.remove('invalid');
+            statusElement.textContent = 'Options saved.';
+            setTimeout(function () {
+                statusElement.textContent = '';
+            }, 2500);
+
+            var bkg = chrome.extension.getBackgroundPage();
+            bkg.settings.proxyUrl = proxyUrl;
+            bkg.settings.enabled = enabled;
+            bkg.settings.urls = urls;
+        });
+    } else {
+        statusElement.classList.add('invalid');
+        statusElement.textContent = 'There are errors with your options.';
+    }
+}
+
+function toggleEnabledDisplay() {
+    var enabled = onOffSwitchElement.checked;
+
+    if (!enabled) {
+        document.getElementById('options').classList.add('disabled');
+    } else {
+        document.getElementById('options').classList.remove('disabled');
+    }
+}
+
+document.addEventListener('DOMContentLoaded', restoreOptions);
+document.getElementById('save').addEventListener('click', saveOptions);
+onOffSwitchElement.addEventListener('click', toggleEnabledDisplay);
